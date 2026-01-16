e8309c809663d65cca6f1e9850ea5cdd9a5bbfaa
Gebu19 <gebu830@gmail.com>
2026-01-16 14:31:12 +0700

    sec commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom' 
 import Home from './components/Home'
-import { Hello1 } from './components/Hello1'
-import Effect from './components/Effect'
 import DashBoard from './components/dashboard'
 
 function App() {
